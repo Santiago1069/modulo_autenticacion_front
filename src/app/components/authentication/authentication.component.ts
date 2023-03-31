cca801ac7ed3baa59b05eb9cacc9dee352cd3a03
Santiago Sandoval <casaba3131@gmail.com>
2023-03-31 18:39:57 -0500

    Se crea la vista del modulo de autenticacion desde se encuentra el login y el registro de usuarios

---
+++ src/app/components/authentication/authentication.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-authentication',
+  templateUrl: './authentication.component.html',
+  styleUrls: ['./authentication.component.css']
+})
+export class AuthenticationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.validacionDeCampos();
+
+  }
+
+  validacionDeCampos(){
+    (() => {
+
+      'use strict'
+      const forms: NodeListOf<HTMLFormElement> = document.querySelectorAll('.needs-validation')
+
+      Array.from(forms).forEach((form: HTMLFormElement) => {
+        form.addEventListener('submit', (event: Event) => {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+
+          form.classList.add('was-validated')
+        }, false)
+      })
+    })()
+  }
+
+}
